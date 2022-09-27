68dfb257121732e99759f63bd3a20c0e39f70c08
keshavkumar <keshavsharma5324@gmail.com>
2022-09-27 12:12:34 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Counter App',
+      title: 'Flutter Counter',
       theme: ThemeData(
         // This is the theme of your application.
         //
